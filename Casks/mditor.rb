38dadce8c8c3bde381e6934643427b9a8c6a81fb
MejiasDev <jose@mejiasdev.com>
2026-07-31 13:20:29 +0200

    depends_on macos: :monterey (style)

--- Casks/mditor.rb
+++ Casks/mditor.rb
@@ -7,7 +7,7 @@ cask "mditor" do
   desc "Native Markdown editor and viewer"
   homepage "https://github.com/mejiasd3v/mditor"
 
-  depends_on macos: ">= :monterey"
+  depends_on macos: :monterey
 
   app "MDitor.app"
 
